804ca232a0895a4d9d4e3b14a61384109274d7ab
ziad <zezo97736@gmail.com>
2025-05-29 21:15:38 +0300

    added dependency injection

--- app/src/main/java/com/outfitgo/store/core/di/AppModule.kt
+++ app/src/main/java/com/outfitgo/store/core/di/AppModule.kt
@@ -1,5 +1,10 @@
 package com.outfitgo.store.core.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import com.apollographql.apollo.ApolloClient
 import com.outfitgo.store.BuildConfig
 import com.outfitgo.store.core.di.qualifiers.AdminApollo
@@ -7,7 +12,13 @@ import com.outfitgo.store.core.di.qualifiers.StorefrontApollo
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
@@ -16,6 +27,7 @@ object AppModule {
 
     const val SERVER_URL = "https://mad45-sv-and3.myshopify.com/api/2025-04/graphql.json"
     const val ADMIN_SERVER_URL = "https://mad45-sv-and3.myshopify.com/admin/api/2025-04/graphql.json"
+    private const val DATASTORE_NAME = "OutfitGo"
 
     @Provides
     @Singleton
@@ -43,5 +55,25 @@ object AppModule {
         }.build()
     }
 
+    @Provides
+    @Singleton
+    fun provideClient(): HttpClient {
+        return HttpClient(CIO){
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
 
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            produceFile = {
+                context.preferencesDataStoreFile(DATASTORE_NAME)
+            }
+        )
+    }
 }
---
+++ app/src/main/java/com/outfitgo/store/core/util/Const.kt
@@ -0,0 +1,5 @@
+package com.outfitgo.store.core.util
+
+object Const {
+    const val CURRENCY_API_URL="https://api.currencyapi.com/v3/latest"
+}
