38fbe298fcc3aeea8920a1939d4effa742ce56ef
3wiida <mahmoudewida5@gmail.com>
2025-05-29 21:24:57 +0300

    make brands and products data sources

--- app/src/main/java/com/outfitgo/store/data/datasource/remote/brand/BrandsRemoteDataSource.kt
+++ app/src/main/java/com/outfitgo/store/data/datasource/remote/brand/BrandsRemoteDataSource.kt
@@ -1,4 +1,10 @@
 package com.outfitgo.store.data.datasource.remote.brand
 
+import com.outfitgo.store.domain.model.brand.Brand
+
 interface BrandsRemoteDataSource {
+    suspend fun fetchAllBrands(
+        first: Int,
+        after: String?
+    ): List<Brand>
 }
--- app/src/main/java/com/outfitgo/store/data/datasource/remote/product/ProductsRemoteDataSource.kt
+++ app/src/main/java/com/outfitgo/store/data/datasource/remote/product/ProductsRemoteDataSource.kt
@@ -1,4 +1,10 @@
 package com.outfitgo.store.data.datasource.remote.product
 
+import com.outfitgo.store.domain.model.product.CommonProduct
+
 interface ProductsRemoteDataSource {
+    suspend fun fetchLatestProducts(
+        first: Int,
+        after: String?
+    ): List<CommonProduct>
 }
