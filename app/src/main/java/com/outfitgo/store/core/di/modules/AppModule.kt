dd9b0c89be9e0a86565a778f021bea7ae1f10c8e
3wiida <mahmoudewida5@gmail.com>
2025-05-29 21:28:02 +0300

    add hilt modules for brands and products

--- app/src/main/java/com/outfitgo/store/core/di/modules/AppModule.kt
+++ app/src/main/java/com/outfitgo/store/core/di/modules/AppModule.kt
@@ -1,9 +1,17 @@
-package com.outfitgo.store.core.di
+package com.outfitgo.store.core.di.modules
 
 import com.apollographql.apollo.ApolloClient
 import com.outfitgo.store.BuildConfig
 import com.outfitgo.store.core.di.qualifiers.AdminApollo
 import com.outfitgo.store.core.di.qualifiers.StorefrontApollo
+import com.outfitgo.store.data.datasource.remote.brand.BrandsRemoteDataSource
+import com.outfitgo.store.data.datasource.remote.brand.BrandsRemoteDataSourceImpl
+import com.outfitgo.store.data.datasource.remote.product.ProductsRemoteDataSource
+import com.outfitgo.store.data.datasource.remote.product.ProductsRemoteDataSourceImpl
+import com.outfitgo.store.data.repository.brand.BrandsRepositoryImpl
+import com.outfitgo.store.data.repository.product.ProductsRepositoryImpl
+import com.outfitgo.store.domain.repository.brand.BrandsRepository
+import com.outfitgo.store.domain.repository.product.ProductsRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -42,6 +50,5 @@ object AppModule {
             )
         }.build()
     }
-
 
 }
---
+++ app/src/main/java/com/outfitgo/store/core/di/modules/BrandsModule.kt
@@ -0,0 +1,29 @@
+package com.outfitgo.store.core.di.modules
+
+import com.apollographql.apollo.ApolloClient
+import com.outfitgo.store.core.di.qualifiers.AdminApollo
+import com.outfitgo.store.data.datasource.remote.brand.BrandsRemoteDataSource
+import com.outfitgo.store.data.datasource.remote.brand.BrandsRemoteDataSourceImpl
+import com.outfitgo.store.data.repository.brand.BrandsRepositoryImpl
+import com.outfitgo.store.domain.repository.brand.BrandsRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object BrandsModule {
+    @Provides
+    @Singleton
+    fun provideBrandsRemoteDataSource(@AdminApollo remoteClient: ApolloClient): BrandsRemoteDataSource {
+        return BrandsRemoteDataSourceImpl(remoteClient)
+    }
+
+    @Provides
+    @Singleton
+    fun provideBrandsRepository(brandsRemoteDataSource: BrandsRemoteDataSource): BrandsRepository{
+        return BrandsRepositoryImpl(brandsRemoteDataSource)
+    }
+}
