1932144a2a9ce930860add92cfda794c3f068d45
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-05-30 16:30:44 +0300

    optimized imports

--- app/src/main/java/com/outfitgo/store/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/outfitgo/store/presentation/ui/theme/Type.kt
@@ -2,9 +2,8 @@ package com.outfitgo.store.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.font.FontFamily
-
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import com.outfitgo.store.R
 
 val provider = GoogleFont.Provider(
