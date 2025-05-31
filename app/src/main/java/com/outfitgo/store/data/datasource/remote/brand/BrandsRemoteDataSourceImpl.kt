1e0ecf8a74d4c391bcf30c1e10a74e274a100176
3wiida <mahmoudewida5@gmail.com>
2025-05-31 15:39:54 +0300

    Add brand products query & setup brands remote data source to fetch brand products

--- app/src/main/java/com/outfitgo/store/data/datasource/remote/brand/BrandsRemoteDataSourceImpl.kt
+++ app/src/main/java/com/outfitgo/store/data/datasource/remote/brand/BrandsRemoteDataSourceImpl.kt
@@ -2,14 +2,17 @@ package com.outfitgo.store.data.datasource.remote.brand
 
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Optional
-import com.outfitgo.store.admin.BrandsQuery
 import com.outfitgo.store.data.mappers.toBrand
+import com.outfitgo.store.data.mappers.toCommonProduct
 import com.outfitgo.store.domain.model.brand.Brand
+import com.outfitgo.store.domain.model.product.CommonProduct
+import com.outfitgo.store.storefront.BrandProductsQuery
+import com.outfitgo.store.storefront.BrandsQuery
 import javax.inject.Inject
 
 class BrandsRemoteDataSourceImpl @Inject constructor(
     private val remoteClient: ApolloClient
-): BrandsRemoteDataSource {
+) : BrandsRemoteDataSource {
 
     override suspend fun fetchAllBrands(
         first: Int,
@@ -18,11 +21,11 @@ class BrandsRemoteDataSourceImpl @Inject constructor(
         val brandsResponse = remoteClient.query(
             BrandsQuery(
                 first = first,
-                after = if(after.isNullOrBlank()) Optional.absent() else Optional.present(after)
+                after = if (after.isNullOrBlank()) Optional.absent() else Optional.present(after)
             )
         ).execute()
 
-        if (brandsResponse.hasErrors()){
+        if (brandsResponse.hasErrors()) {
             throw Exception(brandsResponse.errors?.first()?.message)
         }
 
@@ -33,4 +36,28 @@ class BrandsRemoteDataSourceImpl @Inject constructor(
         return brands
     }
 
+    override suspend fun fetchBrandProducts(
+        brand: String,
+        searchQuery: String,
+        first: Int,
+        after: String?
+    ): List<CommonProduct> {
+        val brandProductsResponse = remoteClient.query(
+            BrandProductsQuery(
+                searchQuery = "vendor:'$brand' title:${if (searchQuery.isBlank()) "*" else "*$searchQuery*"}",
+                first = first,
+                after = if (after.isNullOrBlank()) Optional.absent() else Optional.Present(after)
+            )
+        ).execute()
+
+        if (brandProductsResponse.hasErrors()) {
+            throw Exception(brandProductsResponse.errors?.first()?.message)
+        }
+
+        val brandProducts = brandProductsResponse.dataAssertNoErrors.products.edges.map {
+            it.toCommonProduct()
+        }
+
+        return brandProducts
+    }
 }
