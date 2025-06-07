97d63e0d1e7aaae8904123850b1a6c05d3ebbfeb
ziad <zezo97736@gmail.com>
2025-06-08 02:29:56 +0300

    define cart screen state

---
+++ app/src/main/java/com/outfitgo/store/presentation/cart/CartState.kt
@@ -0,0 +1,14 @@
+package com.outfitgo.store.presentation.cart
+
+import com.outfitgo.store.domain.model.cart.CartItem
+import com.outfitgo.store.domain.model.cart.Cost
+
+data class CartState(
+    val cartItems: List<CartItem> = emptyList(),
+    val isLoading: Boolean = false,
+    val coupon: String = "",
+    val couponMessage: String? = null,
+    val isCouponApplied: Boolean = false,
+    val cartCost: Cost = Cost("0.0"),
+    val error:String? = null
+)
