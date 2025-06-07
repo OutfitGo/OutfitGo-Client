1682ee439cf6d01999daf4a0c0e6e48f773c0eee
ziad <zezo97736@gmail.com>
2025-06-07 17:49:53 +0300

    update cart repository

--- app/src/main/java/com/outfitgo/store/domain/repository/cart/CartRepository.kt
+++ app/src/main/java/com/outfitgo/store/domain/repository/cart/CartRepository.kt
@@ -1,13 +1,16 @@
 package com.outfitgo.store.domain.repository.cart
 
 import com.outfitgo.store.domain.model.cart.Cart
+import com.outfitgo.store.domain.model.cart.Cost
 
 interface CartRepository {
     suspend fun getCart(cartId:String): Cart
-    suspend fun createCart():Cart
-    suspend fun addBuyerToCart(cartId:String,customerAccessToken:String):Cart
-    suspend fun addItemToCart(cartId:String,quantity:Int,productVariantId:String):Cart
-    suspend fun removeItemFromCart(cartId:String,lineId:String):Cart
-    suspend fun updateCartLineQuantity(cartId:String,lineId:String,quantity:Int):Cart
+    suspend fun createCart(): String
+    suspend fun addBuyerToCart(cartId:String,customerAccessToken:String): Boolean
+    suspend fun addItemToCart(cartId:String,quantity:Int,productVariantId:String): Cost
+    suspend fun removeItemFromCart(cartId:String,lineId:String): Cost
+    suspend fun updateCartLineQuantity(cartId:String,lineId:String,quantity:Int): Cost
     suspend fun applyCouponToCart(cartId:String,coupon:String):Cart
+    suspend fun saveCartId(cartId: String)
+    suspend fun getCartId():String
 }
