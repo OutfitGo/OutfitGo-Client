ba5fbd7b6be28f80063d600a1fd4e5631b8be5cf
ziad <zezo97736@gmail.com>
2025-06-08 04:22:12 +0300

    inject local dataSource to cart repository

--- app/src/main/java/com/outfitgo/store/core/di/modules/CartModule.kt
+++ app/src/main/java/com/outfitgo/store/core/di/modules/CartModule.kt
@@ -1,7 +1,11 @@
 package com.outfitgo.store.core.di.modules
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import com.apollographql.apollo.ApolloClient
 import com.outfitgo.store.core.di.qualifiers.StorefrontApollo
+import com.outfitgo.store.data.datasource.local.cart.CartLocalDataSource
+import com.outfitgo.store.data.datasource.local.cart.CartLocalDataSourceImpl
 import com.outfitgo.store.data.datasource.remote.cart.CartRemoteDataSource
 import com.outfitgo.store.data.datasource.remote.cart.CartRemoteDataSourceImpl
 import com.outfitgo.store.data.repository.cart.CartRepositoryImpl
@@ -23,7 +27,16 @@ object CartModule {
 
     @Provides
     @Singleton
-    fun provideCartRepository(remote: CartRemoteDataSource): CartRepository {
-        return CartRepositoryImpl(remote)
+    fun provideCartLocalDataSource(dataStore: DataStore<Preferences>): CartLocalDataSource {
+        return CartLocalDataSourceImpl(dataStore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCartRepository(
+        remote: CartRemoteDataSource,
+        local: CartLocalDataSource
+    ): CartRepository {
+        return CartRepositoryImpl(remote, local)
     }
 }
