b1c8e3dacd31ba50e836a88872c7c3bc210151e7
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-06-09 15:19:19 +0300

    Create Repository for wishlist

---
+++ app/src/main/java/com/outfitgo/store/data/repository/wishlist/WishlistRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.outfitgo.store.data.repository.wishlist
+
+import android.util.Log
+import com.outfitgo.store.data.datasource.remote.wishlist.WishlistRemoteDataSource
+import com.outfitgo.store.domain.model.product.Product
+import com.outfitgo.store.domain.repository.wishilst.WishlistRepository
+import javax.inject.Inject
+
+private const val TAG = "WishlistRepositoryImpl"
+
+class WishlistRepositoryImpl @Inject constructor (
+    private val remoteDataSource: WishlistRemoteDataSource
+): WishlistRepository {
+    override suspend fun addProduct(
+        userId: String,
+        product: Product
+    ) {
+        Log.i(TAG, "addProduct: Started")
+        remoteDataSource.addProduct(userId, product)
+    }
+
+    override suspend fun removeProduct(userId: String, productId: String) {
+        remoteDataSource.removeProduct(userId, productId)
+    }
+
+    override suspend fun getAllProducts(userId: String): List<Product> {
+        return remoteDataSource.getAllProducts(userId)
+    }
+
+    override suspend fun isInWishlist(
+        userId: String,
+        productId: String
+    ): Boolean {
+        return remoteDataSource.isInWishlist(userId, productId)
+    }
+}
