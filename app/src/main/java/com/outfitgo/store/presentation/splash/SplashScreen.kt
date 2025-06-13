2fe25d67d46c7d20b7bdb4c5f3d0855c240e6db8
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-06-14 00:52:50 +0300

    Enhance SplashScreen.kt

--- app/src/main/java/com/outfitgo/store/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/outfitgo/store/presentation/splash/SplashScreen.kt
@@ -1,12 +1,25 @@
 package com.outfitgo.store.presentation.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.outfitgo.store.R
+import com.outfitgo.store.presentation.ui.theme.OutfitGoTheme
 
 
 @Composable
@@ -26,7 +39,44 @@ fun OutfitGoSplashScreen(
         }
     }
 
+    SplashScreenContent(
+        modifier = modifier
+    )
+
+}
+
+
+@Composable
+fun SplashScreenContent(
+    modifier: Modifier = Modifier
+) {
     Box(modifier, contentAlignment = Alignment.Center) {
-        Text("Splash Screen", style = MaterialTheme.typography.displayMedium)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.app_icon),
+                contentDescription = stringResource(R.string.app_name),
+                Modifier
+                    .size(200.dp)
+                    .clip(
+                        CircleShape
+                    )
+            )
+            Text(stringResource(R.string.app_name), style = MaterialTheme.typography.displaySmall)
+        }
     }
 }
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun SplashScreenPreview() {
+    OutfitGoTheme {
+        SplashScreenContent(
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+
