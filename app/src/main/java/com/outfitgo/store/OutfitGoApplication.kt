9c0fb5222fe548964b0ac2d4e9abc1b7d5c991e3
ziad <zezo97736@gmail.com>
2025-06-13 21:48:41 +0300

    add places init

--- app/src/main/java/com/outfitgo/store/OutfitGoApplication.kt
+++ app/src/main/java/com/outfitgo/store/OutfitGoApplication.kt
@@ -2,7 +2,12 @@ package com.outfitgo.store
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import com.google.android.libraries.places.api.Places
 
 @HiltAndroidApp
 class OutfitGoApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Places.initialize(applicationContext, BuildConfig.GOOGLE_MAPS_API_KEY)
+    }
 }
