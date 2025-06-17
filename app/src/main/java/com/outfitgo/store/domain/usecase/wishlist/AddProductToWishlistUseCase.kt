c44a0b68d9e5bc94b97f5df1a16cff5572a076fb
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-06-17 18:02:45 +0300

    remove logs from use cases

--- app/src/main/java/com/outfitgo/store/domain/usecase/wishlist/AddProductToWishlistUseCase.kt
+++ app/src/main/java/com/outfitgo/store/domain/usecase/wishlist/AddProductToWishlistUseCase.kt
@@ -5,14 +5,12 @@ import com.outfitgo.store.domain.model.product.Product
 import com.outfitgo.store.domain.repository.wishilst.WishlistRepository
 import javax.inject.Inject
 
-private const val TAG = "AddProductToWishlistUse"
 
 class AddProductToWishlistUseCase @Inject constructor(
     private val wishlistRepository: WishlistRepository
 ) {
 
     suspend fun execute(userId: String, product: Product) {
-        Log.i(TAG, "execute: started")
         wishlistRepository.addProduct(userId, product)
     }
 }
