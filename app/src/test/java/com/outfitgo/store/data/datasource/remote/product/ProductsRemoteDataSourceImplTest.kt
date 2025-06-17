7d6783e5842505d070f3e8618d5379fc56a4c3db
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-06-17 19:36:22 +0300

    Add Testing For Product Datalayer and useCases for search

---
+++ app/src/test/java/com/outfitgo/store/data/datasource/remote/product/ProductsRemoteDataSourceImplTest.kt
@@ -0,0 +1,17 @@
+package com.outfitgo.store.data.datasource.remote.product
+
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+
+class ProductsRemoteDataSourceImplTest {
+    @Before
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    @After
+    fun tearDown() {
+        TODO("Not yet implemented")
+    }
+}
