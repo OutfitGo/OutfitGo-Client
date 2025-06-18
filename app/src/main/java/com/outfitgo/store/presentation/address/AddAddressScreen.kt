af1ea4db7c2c946ff1f230fa31356515d18bf7ee
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-06-18 17:03:17 +0300

    enhance form by adding single line to all fields

--- app/src/main/java/com/outfitgo/store/presentation/address/AddAddressScreen.kt
+++ app/src/main/java/com/outfitgo/store/presentation/address/AddAddressScreen.kt
@@ -3,12 +3,15 @@ package com.outfitgo.store.presentation.address
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -76,6 +79,8 @@ fun AddAddressScreen(
                 value = firstName,
                 onValueChange = { firstName = it },
                 label = { Text("First Name") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next),
                 modifier = Modifier.fillMaxWidth()
             )
 
@@ -83,13 +88,18 @@ fun AddAddressScreen(
                 value = lastName,
                 onValueChange = { lastName = it },
                 label = { Text("Last Name") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next),
                 modifier = Modifier.fillMaxWidth()
+
             )
 
             OutlinedTextField(
                 value = addressLine,
                 onValueChange = { addressLine = it },
                 label = { Text("Address Line") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Done),
                 modifier = Modifier.fillMaxWidth()
             )
             ExposedDropdownMenuBox(
