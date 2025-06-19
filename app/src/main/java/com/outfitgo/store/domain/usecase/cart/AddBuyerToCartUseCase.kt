84f7763a4d0a17d0352bfdfd556608d567a687cb
ziad <zezo97736@gmail.com>
2025-06-19 12:27:07 +0300

    fix add user to cart issue

--- app/src/main/java/com/outfitgo/store/domain/usecase/cart/AddBuyerToCartUseCase.kt
+++ app/src/main/java/com/outfitgo/store/domain/usecase/cart/AddBuyerToCartUseCase.kt
@@ -1,5 +1,6 @@
 package com.outfitgo.store.domain.usecase.cart
 
+import android.util.Log
 import com.outfitgo.store.domain.repository.cart.CartRepository
 import com.outfitgo.store.domain.repository.user.UsersRepository
 import javax.inject.Inject
@@ -9,6 +10,7 @@ class AddBuyerToCartUseCase @Inject constructor(
     private val usersRepository: UsersRepository
 ) {
     suspend fun execute(cartId: String): Boolean {
+        Log.d("``TAG``", "execute: user token ${usersRepository.getSavedUserToken()} ")
         return cartRepository.addBuyerToCart( cartId,usersRepository.getSavedUserToken()?:"")
     }
 }
